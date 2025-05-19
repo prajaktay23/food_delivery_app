e14aa4fd8723d329696693cc6126f0f6927d93f2
prajaktay23 <prajaktayadav525@gmail.com>
2025-05-19 08:11:18 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView style={{backgroundColor: 'pink', flex: 1}}>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
